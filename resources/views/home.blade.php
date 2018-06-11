fb009b1c5ccb299d1fe71daff6bcf59a785b4b67
Антон <toxa4elovek@gmail.com>
2018-06-11 14:08:42 +0300

    change assets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        @include('elements.category', ['categories' => ['Посты', 'Новости']])
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+        @include('elements.posts')
+
     </div>
 </div>
 @endsection
